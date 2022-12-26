8d3bde745fe73c81d803e713a4f3f729e77dbb93
JayMehta711 <jaymehta711@gmail.com>
2022-12-26 11:31:24 +0530

    Package file updated

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(name: "Grocerybag",
-                      url: "https://github.com/JayMehta711/Grocerybag.git",
+                      url: "https://github.com/JayMehta711/Grocerybag.zip",
                       checksum: "7f2420a7abac7259b77ce502ba7bccc4eb089c8c")
     ]
 )
